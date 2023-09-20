0d6c62905d945695dfd321646e8362654764964f
sere06001 <sere06001@utb.vaxjo.se>
2023-09-20 10:20:42 +0200

    Add project files.

---
+++ Uppgift 2.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Uppgift2_2
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Hur långt hoppade Elin i meter?");
+            double elin = double.Parse(Console.ReadLine());
+            Console.WriteLine("Hur långt hoppade Alma i meter?");
+            double alma = double.Parse(Console.ReadLine());
+            double skillnad = elin - alma;
+            Console.WriteLine("Elin hoppade " + skillnad + " meter längre.");
+            Console.ReadKey();
+        }
+    }
+}
